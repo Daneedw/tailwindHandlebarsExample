317adf84bd7466f9ef5dff27565cdccd1ebaf0af
Dane Edwards <Daneedw@gmail.com>
2023-09-07 11:55:17 -0400

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{handlebars,html,js}"],
+  content: ["./views/**/*.{handlebars,html,js}","./public/js/**/*.{handlebars,html,js}"],
   theme: {
     extend: {},
   },
